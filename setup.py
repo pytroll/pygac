e287992801d4d8ba3e4ace78467190623374fce4
Adam Dybbroe <adam.dybbroe@smhi.se>
2014-05-06 09:23:05 +0200

    Added h5py as a requirement in setup

--- setup.py
+++ setup.py
@@ -55,7 +55,8 @@ setup(name='pygac',
       # Project should use reStructuredText, so ensure that the docutils get
       # installed or upgraded on the target machine
       install_requires=['docutils>=0.3', 
-                        'numpy', 'pyorbital>=v0.3.2'],
+                        'numpy', 'pyorbital>=v0.3.2',
+                        'h5py'],
       extras_require = {'geolocation interpolation': ['python-geotiepoints'],
                         },
       scripts = [],      
