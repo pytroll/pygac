6bf11d7026600771eb62dd38f79b3b17848db906
Stephan Finkensieper <stephan.finkensieper@dwd.de>
2017-01-17 12:12:54 +0100

    Add tests for timestamp decoding

---
+++ pygac/tests/test_reader.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Author(s):
+
+#   Stephan Finkensieper <stephan.finkensieper@dwd.de>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+import numpy as np
+from pygac.gac_reader import GACReader
+
+
+class TestGacReader(unittest.TestCase):
+    """Test the common GAC Reader"""
+
+    longMessage = True
+
+    def test_to_datetime64(self):
+        """Test conversion from (year, jday, msec) to datetime64"""
+        t0 = GACReader.to_datetime64(year=np.array(1970), jday=np.array(1),
+                                     msec=np.array(0))
+        self.assertEqual(t0.astype('i8'), 0,
+                         msg='Conversion from (year, jday, msec) to datetime64 '
+                             'is not correct')
+
+if __name__ == '__main__':
+    unittest.main()
