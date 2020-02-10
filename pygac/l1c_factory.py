b70b9e7aa367c0656b4aaca4363df5a65fcb6e24
Carlos <mattescarlos@gmail.com>
2020-02-10 21:24:27 +0100

    added Builder class

--- pygac/l1c_factory.py
+++ pygac/l1c_factory.py
@@ -23,22 +23,63 @@
 """Factory for l1c files.
 
 """
+import datetime
 import logging
 
+from pygac.gac_klm import GACKLMReader
+from pygac.gac_pod import GACPODReader
+from pygac.lac_klm import LACKLMReader
+from pygac.lac_pod import LACPODReader
 from pygac.utils import file_opener
+from pygac.gac_io import save_gac
+
 
 LOG = logging.getLogger(__name__)
 
+class L1cBuilder(object):
+    def __init__(self, Reader):
+        self.Reader = Reader
+    
+    def __call__(filename, start_line, end_line, fileobj=None):
+        tic = datetime.datetime.now()
+        reader = self.Reader.fromfile(filename, fileobj=fileobj)
+        self._save(reader)
+        LOG.info("pygac took: %s", str(datetime.datetime.now() - tic))
+    
+    def _save(self, reader):
+        channels = reader.get_calibrated_channels()
+        sat_azi, sat_zen, sun_azi, sun_zen, rel_azi = reader.get_angles()
+        qual_flags = reader.get_qual_flags()
+        if (np.all(reader.mask)):
+            LOG.error("All data is masked out. Stop processing!")
+            raise ValueError("All data is masked out.")
+        save_gac(
+            reader.spacecraft_name, reader.utcs,
+            reader.lats, reader.lons,
+            channels[:, :, 0], channels[:, :, 1],
+            channels[:, :, 2], channels[:, :, 3],
+            channels[:, :, 4], channels[:, :, 5],
+            sun_zen, sat_zen, sun_azi, sat_azi, rel_azi,
+            qual_flags, start_line, end_line,
+            reader.filename, reader.meta_data
+        )
+
 class L1cFactory(object):
     """Factory for level 1c files."""
-    def __call__(self, filename, start_line, end_line):
-        # check file version
-        processor = self.check_file_version
+    __builders = {
+        "gac_klm": L1cBuilder(GACKLMReader),
+        "gac_pod": L1cBuilder(GACPODReader),
+        "lac_klm": L1cBuilder(LACKLMReader),
+        "lac_pod": L1cBuilder(LACPODReader)
+    }
+    def __call__(self, filename, start_line, end_line, fileobj=None):
+        # check which builder to use
+        builder = self.get_builder(filename, fileobj=fileobj)
         # process
-        processor(filename, start_line, end_line)
+        builder(filename, start_line, end_line, fileobj=None)
     
-    @staticmethod
-    def check_file_version(filename):
+    @classmethod
+    def get_builder(cls, filename):
         try:
             with file_opener(filename) as fdes:
                 try:
@@ -54,9 +95,8 @@ class L1cFactory(object):
         # who can read it (would require to preload the classes)
         # which seems to be avoided by the developers
         # At least, this desission does not belong into the run script!
+        # TODO: How do we determine if we need the lac_klm or lac_pod builder?
         if data in ["CMS", "NSS", "UKM", "DSS"]:
-            from pygac.gac_klm import main
-            return main
+            return cls.__builders["gac_klm"]
         else:
-            from pygac.gac_pod import main
-            return main
+            return cls.__builders["gac_pod"]
